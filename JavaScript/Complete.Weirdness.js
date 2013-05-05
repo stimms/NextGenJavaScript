0f69fc3cc7c1ccd8a97520f370552e144f156086
Simon Timms <simon.timms@worleyparsons.com>
2013-05-04 21:28:33 -0600

    Initial add

---
+++ JavaScript/Complete.Weirdness.js
@@ -0,0 +1,13 @@
+console.log(NaN == NaN);
+console.log(typeof NaN);
+console.log(NaN instanceof Number);
+
+console.log(typeof null);
+console.log(null instanceof Object); 
+
+var emptyArray = []; 
+console.log(emptyArray == false); 
+if (emptyArray) console.log('empty array is true');
+
+[] + []
+[] + {}
---
+++ TypeScript/Complete.Boat.ts
@@ -0,0 +1,10 @@
+module Vehicles.OceanGoing
+{
+	export class Boat
+	{
+		constructor(public length: number, public name: string)
+		{
+
+		}
+	}
+}
